055294626ffdb9ea7e4ec74b4b4f684b3f678d48
Goro-K <romankizilt@gmail.com>
2024-02-19 11:57:16 +0100

    commit project

---
+++ back/middleware/multerConfig.js
@@ -0,0 +1,38 @@
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const directoryName = path.dirname(fileURLToPath(import.meta.url)); 
+
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
+  };
+
+
+const storage= multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, path.join(directoryName, '../images'));
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_');
+        const extension = MIME_TYPES[file.mimetype]
+        callback(null, name + Date.now() + "." + extension);
+    }
+})
+
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, callback) => {
+        if(MIME_TYPES[file.mimetype]) {
+            callback(null, true)
+        } else {
+           return callback(new Error('Please upload a file type of jpeg or png'))
+        }
+    }
+});
+
+const image = upload.single('image')
+
+export default image;
